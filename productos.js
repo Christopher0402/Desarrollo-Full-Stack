d836672ab4c96d8b56aec56a3f0582407de10684
Christopher <educhris0402@gmail.com>
2026-02-17 17:36:39 -0600

    Delete productos.js

--- productos.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../middleware/auth')
-const { crearProducto, obtenerProductos } = require('../controllers/productoController')
-
-router.post('/', auth, crearProducto)
-router.get('/', auth, obtenerProductos)
-
-module.exports = router
